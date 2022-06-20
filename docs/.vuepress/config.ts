215c99782030c510c9ea9ae1b9ff30dea2048595
asiyeaili <88175568+sarwaras@users.noreply.github.com>
2022-06-20 15:46:17 +0800

    插件

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -10,7 +10,9 @@
 import { defineUserConfig } from "vuepress"
 import { path } from "@vuepress/utils"
 import { defaultTheme } from "@vuepress/theme-default"
-import KnznTheme from "vuepress-theme-knzn"
+// import KnznTheme from "vuepress-theme-knzn"
+
+import { googleAnalyticsPlugin } from "@vuepress/plugin-google-analytics"
 
 export default defineUserConfig({
   lang: "zh-CN",
@@ -37,20 +39,20 @@ export default defineUserConfig({
     },
   },
 
-  //  默认主题的配置
+  //  官网的默认主题
   //  语言切换的配置
-  // theme: defaultTheme({
-  //   locales: {
-  //     "/": {
-  //       selectLanguageName: "English",
-  //       selectLanguageText: "Languages",
-  //     },
-  //     "/zh/": {
-  //       selectLanguageName: "简体中文",
-  //       selectLanguageText: "选择语言",
-  //     },
-  //   },
-  // }),
+  theme: defaultTheme({
+    locales: {
+      "/": {
+        selectLanguageName: "English",
+        selectLanguageText: "Languages",
+      },
+      "/zh/": {
+        selectLanguageName: "简体中文",
+        selectLanguageText: "选择语言",
+      },
+    },
+  }),
 
   //  markdown 的配置
   markdown: {
@@ -65,68 +67,78 @@ export default defineUserConfig({
       handleImportPath: str => str.replace(/^@vuepress/, path.resolve(__dirname, "./")),
     },
   },
-  theme: KnznTheme({
-    // logo
-    logo: "/images/logo-light.svg",
-    darkLogo: "/images/logo-dark.svg",
-    // 背景图片
-    backgroundImage: "/images/bg.svg",
-    darkBackgroundImage: "/images/bg-dark.jpg",
-    // 文章简介图片
-    // postImage: 'https://v2.vuepress.vuejs.org/images/hero.png',
-    darkPostImage: "/images/post-dark.svg",
-    // 背景canvas 动画配置
-    // particlesOptions,
 
-    /**
-     * 博主信息相关
-     */
-    // 博主名称
-    blogger: "前端程序猿",
-    // 铭言
-    slogan: "种一棵树最好的时间是十年前，其次是现在！",
-    // 头像
-    avatar: "/images/avatar.jpg",
-    darkAvatar: "/images/avatar-dark.jpg",
-    // 其它媒体
-    medias: [
-      { link: "mailto:1140457303@qq.com", icon: "email" },
-      {
-        link: "http://wpa.qq.com/msgrd?v=3&uin=1140457303&site=qq&menu=yes",
-        icon: "QQ",
-      },
-      { link: "https://www.jianshu.com/u/f22487800f07", icon: "jianshu" },
-      { link: "https://gitee.com/zhanghuanbiao", icon: "gitee" },
-      { link: "https://github.com/zhb333", icon: "github" },
-    ],
+  //  博主自己写的主题  （暂时注释掉）
+  // theme: KnznTheme({
+  //   // logo
+  //   logo: "/images/logo-light.svg",
+  //   darkLogo: "/images/logo-dark.svg",
+  //   // 背景图片
+  //   backgroundImage: "/images/bg.svg",
+  //   darkBackgroundImage: "/images/bg-dark.jpg",
+  //   // 文章简介图片
+  //   // postImage: 'https://v2.vuepress.vuejs.org/images/hero.png',
+  //   darkPostImage: "/images/post-dark.svg",
+  //   // 背景canvas 动画配置
+  //   // particlesOptions,
 
-    /**
-     * 数据显示相关
-     */
-    // 列表页显示的文章个数
-    perPage: 10,
-    // 主页显示分类的个数
-    maxCategories: 6,
-    // 主页显示的标签个数
-    maxTags: 10,
+  //   /**
+  //    * 博主信息相关
+  //    */
+  //   // 博主名称
+  //   blogger: "前端程序猿",
+  //   // 铭言
+  //   slogan: "种一棵树最好的时间是十年前，其次是现在！",
+  //   // 头像
+  //   avatar: "/images/avatar.jpg",
+  //   darkAvatar: "/images/avatar-dark.jpg",
+  //   // 其它媒体
+  //   medias: [
+  //     { link: "mailto:1140457303@qq.com", icon: "email" },
+  //     {
+  //       link: "http://wpa.qq.com/msgrd?v=3&uin=1140457303&site=qq&menu=yes",
+  //       icon: "QQ",
+  //     },
+  //     { link: "https://www.jianshu.com/u/f22487800f07", icon: "jianshu" },
+  //     { link: "https://gitee.com/zhanghuanbiao", icon: "gitee" },
+  //     { link: "https://github.com/zhb333", icon: "github" },
+  //   ],
 
-    // 导航
-    // navbar: [],
+  //   /**
+  //    * 数据显示相关
+  //    */
+  //   // 列表页显示的文章个数
+  //   perPage: 10,
+  //   // 主页显示分类的个数
+  //   maxCategories: 6,
+  //   // 主页显示的标签个数
+  //   maxTags: 10,
 
-    /**
-     *  页脚相关配置
-     */
-    // 备案号
-    beian: "粤ICP备20016112号-2",
-    // 备案查询地址
-    beianUrl: "",
-    // 网址起始时间
-    siteStartDate: "2018",
-    comment: {
-      repo: "zhb333/vuepress-theme-knzn",
-      repoId: "R_kgDOHSwV_A",
-      category: "General",
-      categoryId: "DIC_kwDOHSwV_M4CPHIA",
-    },
-  }),
+  //   // 导航
+  //   // navbar: [],
+
+  //   /**
+  //    *  页脚相关配置
+  //    */
+  //   // 备案号
+  //   beian: "粤ICP备20016112号-2",
+  //   // 备案查询地址
+  //   beianUrl: "",
+  //   // 网址起始时间
+  //   siteStartDate: "2018",
+  //   comment: {
+  //     repo: "zhb333/vuepress-theme-knzn",
+  //     repoId: "R_kgDOHSwV_A",
+  //     category: "General",
+  //     categoryId: "DIC_kwDOHSwV_M4CPHIA",
+  //   },
+  // }),
+
+  //  插件的配置
+
+  plugins: [
+    googleAnalyticsPlugin({
+      id: "G-xxxxxxxxxxxx",
+    }),
+  ],
 })
